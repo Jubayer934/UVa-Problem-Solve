8ffeba978e78631535a0fac9a434c19da001e2da
Jubayer934 <mdnoshin62@gmail.com>
2023-05-09 01:20:48 +0600

    p11172 solve by cpp

---
+++ p11172.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    int n;
+    cin>>n;
+    while(n--)
+    {
+           cin>>num1>>num2;
+           if(num1>num2)   
+              cout<<">"<<endl;
+           else if(num1<num2)
+              cout<<"<"<<endl;
+           else  
+              cout<<"="<<endl;
+    }
+ 
+}
